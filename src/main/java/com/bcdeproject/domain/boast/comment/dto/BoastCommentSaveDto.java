22f2bf79d47110bba46c0a572cdc9036a7ac35e4
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-27 02:56:09 +0900

    feat : BoastComment 관련 Dto Swagger 설정 추가

--- src/main/java/com/bcdeproject/domain/boast/comment/dto/BoastCommentSaveDto.java
+++ src/main/java/com/bcdeproject/domain/boast/comment/dto/BoastCommentSaveDto.java
@@ -1,11 +1,18 @@
 package com.bcdeproject.domain.boast.comment.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
+@ApiModel(description = "게시물 댓글 저장 항목")
 public class BoastCommentSaveDto {
 
+    @ApiModelProperty(value = "게시물 댓글 등록 내용", required = true)
+    @NotBlank(message = "등록할 댓글의 내용을 입력하세요.")
     private String content;
 }
