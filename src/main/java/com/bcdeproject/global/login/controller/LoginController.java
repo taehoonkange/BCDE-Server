ad22014620cb0c0c97136d6edfd36f7701852541
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-27 02:26:16 +0900

    feat : LoginController Swagger 설정 추가

--- src/main/java/com/bcdeproject/global/login/controller/LoginController.java
+++ src/main/java/com/bcdeproject/global/login/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.bcdeproject.global.login.controller;
 
 import com.bcdeproject.global.login.dto.LoginDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,13 @@ public class LoginController {
 
     // Login 로직은 JsonUsernamePasswordAuthenticationFilter에서 진행
     // Swagger용 API
+    @Operation(summary = "로그인 API", description = "로그인 API Example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "404", description = "회원을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러"),
+    })
     @PostMapping("/login")
     public void login(@RequestBody LoginDto loginDto) {
         log.info("요청 로그인 username : {}", loginDto.getUsername());
