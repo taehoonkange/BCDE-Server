2c0892b0ddbab033dbff1bc99362d3038da5858d
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-26 13:47:35 +0900

    feat : 로그인 실패 메시지 추가

--- src/main/java/com/bcdeproject/global/login/handler/LoginFailureHandler.java
+++ src/main/java/com/bcdeproject/global/login/handler/LoginFailureHandler.java
@@ -1,9 +1,12 @@
 package com.bcdeproject.global.login.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
+import javax.security.auth.login.LoginException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +21,7 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.getWriter().write("fail");
-        log.info("로그인에 실패했습니다");
+        response.getWriter().write("check username or password");
+        log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
     }
 }
