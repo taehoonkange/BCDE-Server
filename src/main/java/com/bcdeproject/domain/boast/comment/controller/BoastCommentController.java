5c9a5494e6a9fb2c2c678945078c8b18e90b363a
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-27 02:55:38 +0900

    feat : BoastCommentController Swagger 설정 추가

--- src/main/java/com/bcdeproject/domain/boast/comment/controller/BoastCommentController.java
+++ src/main/java/com/bcdeproject/domain/boast/comment/controller/BoastCommentController.java
@@ -3,6 +3,10 @@ package com.bcdeproject.domain.boast.comment.controller;
 import com.bcdeproject.domain.boast.comment.dto.BoastCommentSaveDto;
 import com.bcdeproject.domain.boast.comment.dto.BoastCommentUpdateDto;
 import com.bcdeproject.domain.boast.comment.service.BoastCommentService;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +21,15 @@ public class BoastCommentController {
     /**
      * 댓글 저장
      */
+    @Operation(summary = "자랑 게시물 댓글 저장 API", description = "자랑 게시물 댓글 저장 API Example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러"),
+    })
     @PostMapping("{boastPostId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void commentSave(@PathVariable("boastPostId") Long boastPostId,
+    public void commentSave(@PathVariable("boastPostId") @ApiParam(value = "자랑 게시물 식별 ID") Long boastPostId,
                             @RequestBody BoastCommentSaveDto boastCommentSaveDto) {
         boastCommentService.save(boastPostId, boastCommentSaveDto);
     }
@@ -27,10 +37,16 @@ public class BoastCommentController {
     /**
      * 대댓글 저장
      */
+    @Operation(summary = "자랑 게시물 대댓글 저장 API", description = "자랑 게시물 대댓글 저장 API Example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러"),
+    })
     @PostMapping("{boastPostId}/{boastCommentId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void reCommentSave(@PathVariable("boastPostId") Long boastPostId,
-                              @PathVariable("boastCommentId") Long boastCommentId,
+    public void reCommentSave(@PathVariable("boastPostId") @ApiParam(value = "자랑 게시물 식별 ID") Long boastPostId,
+                              @PathVariable("boastCommentId") @ApiParam(value = "자랑 게시물 댓글 식별 ID") Long boastCommentId,
                               @RequestBody BoastCommentSaveDto boastCommentSaveDto) {
         boastCommentService.saveReComment(boastPostId, boastCommentId, boastCommentSaveDto);
     }
@@ -38,8 +54,14 @@ public class BoastCommentController {
     /**
      * 댓글 업데이트
      */
+    @Operation(summary = "자랑 게시물 댓글 수정 API", description = "자랑 게시물 댓글 수정 API Example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러"),
+    })
     @PatchMapping("{boastCommentId}")
-    public void update(@PathVariable("boastCommentId") Long boastCommentId,
+    public void update(@PathVariable("boastCommentId") @ApiParam(value = "자랑 게시물 댓글 식별 ID") Long boastCommentId,
                        @RequestBody BoastCommentUpdateDto boastCommentUpdateDto) {
         boastCommentService.update(boastCommentId, boastCommentUpdateDto);
     }
@@ -47,8 +69,14 @@ public class BoastCommentController {
     /**
      * 댓글 삭제
      */
+    @Operation(summary = "자랑 게시물 댓글 삭제 API", description = "자랑 게시물 댓글 삭제 API Example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러"),
+    })
     @DeleteMapping("{boastCommentId}")
-    public void delete(@PathVariable("boastCommentId") Long boastCommentId) {
+    public void delete(@PathVariable("boastCommentId") @ApiParam(value = "자랑 게시물 댓글 식별 ID") Long boastCommentId) {
         boastCommentService.remove(boastCommentId);
     }
 }
