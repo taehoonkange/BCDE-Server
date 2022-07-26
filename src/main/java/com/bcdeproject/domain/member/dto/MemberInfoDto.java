3338977cd4eea57a21fee2334c42bce90c9bc542
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-27 02:26:43 +0900

    feat : MemberDto Swagger 설정 추가

--- src/main/java/com/bcdeproject/domain/member/dto/MemberInfoDto.java
+++ src/main/java/com/bcdeproject/domain/member/dto/MemberInfoDto.java
@@ -1,16 +1,24 @@
 package com.bcdeproject.domain.member.dto;
 
 import com.bcdeproject.domain.member.Member;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@ApiModel(description = "회원 조회 결과 항목")
 public class MemberInfoDto {
 
+    @ApiModelProperty(value = "회원 ID", required = true)
     private String username;
+
+    @ApiModelProperty(value = "회원 닉네임", required = true)
     private String nickName;
+
+    @ApiModelProperty(value = "회원 프로필 사진 URL", required = true)
     private String profileImgUrl;
 
     @Builder
