4674871a084c00612fd55d4f3537315dc90b00ed
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-27 02:43:21 +0900

    feat : BoastPost 관련 Dto Swagger 설정 추가

--- src/main/java/com/bcdeproject/domain/boast/post/dto/BoastPostSaveDto.java
+++ src/main/java/com/bcdeproject/domain/boast/post/dto/BoastPostSaveDto.java
@@ -4,6 +4,8 @@ import com.bcdeproject.domain.boast.hashtag.BoastHashTag;
 import com.bcdeproject.domain.boast.hashtag.dto.BoastHashTagDto;
 import com.bcdeproject.domain.boast.imgurl.dto.BoastImgUrlDto;
 import com.bcdeproject.domain.boast.post.BoastPost;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +17,18 @@ import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
+@ApiModel(description = "게시물 저장 항목")
 public class BoastPostSaveDto {
 
+    @ApiModelProperty(value = "등록할 게시물 제목", required = true)
     @NotBlank(message = "제목을 입력해주세요.")
     private String title;
 
+    @ApiModelProperty(value = "등록할 게시물 내용", required = true)
     @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
+    @ApiModelProperty(value = "등록할 게시물 해시태그 리스트", required = true)
+    @NotBlank(message = "해시태그를 입력해주세요.")
     private List<BoastHashTagDto> hashTag;
 }
