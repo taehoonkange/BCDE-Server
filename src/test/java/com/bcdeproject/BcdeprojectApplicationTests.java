43a4aa17f294ff163e7a98e1f8130651119ce533
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-07-29 17:35:54 +0900

    feat : test aws.yml 분리

--- src/test/java/com/bcdeproject/BcdeprojectApplicationTests.java
+++ src/test/java/com/bcdeproject/BcdeprojectApplicationTests.java
@@ -2,11 +2,12 @@ package com.bcdeproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(properties = "spring.config.location=" +
 		"classpath:/application.yml" +
-		",classpath:/aws.yml"
+		",classpath:/aws-test.yml"
 )
 class BcdeprojectApplicationTests {
 
